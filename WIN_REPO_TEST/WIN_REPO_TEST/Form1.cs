9115566052226961646a87794e3157c5e4eca15f
Krzysztof Grabek <krzysztofgrabek@gmail.com>
2020-03-22 13:00:50 +0100

    cos

--- WIN_REPO_TEST/WIN_REPO_TEST/Form1.cs
+++ WIN_REPO_TEST/WIN_REPO_TEST/Form1.cs
@@ -16,5 +16,10 @@ namespace WIN_REPO_TEST
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
